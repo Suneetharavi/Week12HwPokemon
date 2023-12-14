be5b2c77b031da99f99d4dfa9c64dc2238961caf
Ravikumar Thota <suneetha.gopu3@gmailcom>
2023-12-14 17:12:22 -0500

    Added dynamic anchor tags to index.jsx

--- views/Index.jsx
+++ views/Index.jsx
@@ -13,18 +13,15 @@ function Index(props) {
     <div style={myStyle}><h1>My First React Component!</h1> </div>
     <ul>
         {props.pokemon.map((poke,i) =>{
-            return <li> <h3>{poke.name.charAt(0).toUpperCase()+poke.name.substring(1)}</h3></li>
+            return <li> 
+                        <a href={`/pokemon/${i}`}>
+                        <h3>{poke.name.charAt(0).toUpperCase()+poke.name.substring(1)}</h3>
+                        </a>
+                </li>
         
         }
         )}
-
-
-
-    </ul>
-    
-    
-    
-   
+    </ul>   
     </>
   )
 }
