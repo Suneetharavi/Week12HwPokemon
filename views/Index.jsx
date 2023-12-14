340b44bb55f07cfb91b463f752843ec68ec6fd74
Ravikumar Thota <suneetha.gopu3@gmailcom>
2023-12-14 17:03:45 -0500

    I can view a list of all my pokemon in the browser

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import pokemon from '../models/pokemon';
 
-function Index() {
+function Index(props) {
 
     const myStyle = {
         color: '#ffffff',
@@ -8,7 +9,23 @@ function Index() {
         };
         
   return (
-    <div style={myStyle}><h1>My First React Component!</h1></div>
+    <>
+    <div style={myStyle}><h1>My First React Component!</h1> </div>
+    <ul>
+        {props.pokemon.map((poke,i) =>{
+            return <li> <h3>{poke.name.charAt(0).toUpperCase()+poke.name.substring(1)}</h3></li>
+        
+        }
+        )}
+
+
+
+    </ul>
+    
+    
+    
+   
+    </>
   )
 }
 
